e47f83a8a4087fa8682f8922bb7067998d54f1e1
Nabs-1 <nabilah.aralepo@gmail.com>
2025-04-16 19:50:51 -0500

    Update docker.tf

--- docker.tf
+++ docker.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
-  region = "us-east-1"
-  profile= "yusuf"
+  region = "us-east-2"
+  profile= "Nabilah"
 }
 
 # Create a remote backend for your terraform 
@@ -10,7 +10,7 @@ terraform {
     bucket = "austinobioma-docker-tfstate"
     dynamodb_table = "app-state"
     key    = "LockID"
-    region = "us-east-1"
+    region = "us-east-2"
     profile = "austinobioma-realcloud"
   }
 }
@@ -116,7 +116,7 @@ resource "aws_instance" "ec2_instance1" {
   instance_type          = "t2.micro"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "Feb-Class"
+  key_name               = "terra"
   user_data            = "${file("docker-install.sh")}"
 
   tags = {
